f1d7e2ae8e846d3112a151413f564ec85b0e907a
Dennis Wanjiru <denniswanjiru71@gmail.com>
2023-08-21 21:47:40 +0300

    users can view all classes in various categorie

--- src/app/dashboard/explore/page.tsx
+++ src/app/dashboard/explore/page.tsx
@@ -1,5 +1,79 @@
-import React from "react";
+import getCurrentUser from "@/actions/getCurrentUser";
+import ClassCard from "@/components/ClassCard";
+import { Class } from "@/lib/types";
+import { createAvatarUrl } from "@/lib/utils";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import Supa from "@/assets/supaman.jpeg";
+import Link from "next/link";
 
-export default function Index() {
-  return <div>Explore</div>;
+export default async function Index() {
+  const supabase = createServerComponentClient({ cookies });
+  const currentUser = await getCurrentUser();
+
+  const { data } = await supabase
+    .from("category")
+    .select(`name, class(id, code, name, thumbnail, instructor_id)`);
+
+  const categories = data as { name: string; class: Class[] }[] | null;
+
+  const { data: enrollments } = await supabase
+    .from("enrollment")
+    .select(`id, class(id)`)
+    .eq("student_id", currentUser?.id ?? "");
+
+  const currentUserClasses = enrollments
+    ? // @ts-ignore
+      (enrollments.map((enrollment) => enrollment.class?.id) as string[])
+    : [];
+
+  return (
+    <div>
+      <h2 className="font-bold text-2xl">Explore All Classes</h2>
+
+      {categories
+        ? categories?.map(({ name, class: classes }) => (
+            <>
+              {classes.length ? (
+                <section className="mt-10">
+                  <div className="flex justify-between items-center">
+                    <h3 className=" font-semibold text-base">{name}</h3>
+                    {classes.length > 3 ? (
+                      <Link href="#" className="text-blue-800">
+                        See More
+                      </Link>
+                    ) : null}
+                  </div>
+                  <div className="flex space-x-6">
+                    {classes
+                      ? classes
+                          .slice(0, 2)
+                          .map(
+                            ({ id, code, name, thumbnail, instructor_id }) => (
+                              <ClassCard
+                                key={id}
+                                code={code}
+                                name={name}
+                                type={
+                                  !currentUserClasses.includes(id)
+                                    ? "enroll"
+                                    : undefined
+                                }
+                                thumbnail={
+                                  thumbnail ? createAvatarUrl(thumbnail) : Supa
+                                }
+                                instructorId={instructor_id}
+                                className="mt-6"
+                              />
+                            )
+                          )
+                      : null}
+                  </div>
+                </section>
+              ) : null}
+            </>
+          ))
+        : null}
+    </div>
+  );
 }
--- src/components/ClassCard.tsx
+++ src/components/ClassCard.tsx
@@ -1,18 +1,24 @@
+import { cn } from "@/lib/utils";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Image from "next/image";
+import Button from "./Button";
 
 type Props = {
   thumbnail: any;
   code: string;
   name: string;
+  type?: "enroll" | "normal";
   instructorId: string;
+  className?: string;
 };
 
 export default async function ClassCard({
   thumbnail,
   name,
   code,
+  type,
+  className,
   instructorId,
 }: Props) {
   const supabase = createServerComponentClient({ cookies });
@@ -24,7 +30,12 @@ export default async function ClassCard({
   const instructor = instructors ? instructors[0] : null;
 
   return (
-    <div className="card card-side shadow-md bg-secondary h-40">
+    <div
+      className={cn(
+        "card card-side shadow-md bg-secondary h-40 w-1/3 max-w-sm",
+        className
+      )}
+    >
       <figure className="w-1/3">
         <Image
           width={100}
@@ -34,9 +45,14 @@ export default async function ClassCard({
           className="object-cover h-full"
         />
       </figure>
-      <div className="card-body">
+      <div
+        className={cn("card-body justify-center", {
+          "py-4": type === "enroll",
+        })}
+      >
         <h4 className="invert-[.3]">{code}</h4>
         <h3 className="card-title truncate invert-[.2]">{name}</h3>
+
         {instructor ? (
           <p className="text-sm truncate">
             Instructor:{" "}
@@ -44,6 +60,15 @@ export default async function ClassCard({
               {instructor.prefix}. {instructor.name}{" "}
             </span>
           </p>
+        ) : null}
+
+        {type === "enroll" ? (
+          <div className="w-20">
+            <Button
+              title="Enroll"
+              className="h-7 bg-primary text-xs rounded-md"
+            />
+          </div>
         ) : null}
       </div>
     </div>
