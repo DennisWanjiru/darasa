c1e97f0f072baa611f3718e67d852cad32bcba1f
Dennis Wanjiru <denniswanjiru71@gmail.com>
2023-08-29 14:43:01 +0300

    add loader to the class details page

--- src/app/dashboard/classes/[id]/page.tsx
+++ src/app/dashboard/classes/[id]/page.tsx
@@ -5,6 +5,7 @@ import AddGradeModal from "@/components/modals/AddGradeModal";
 import { Enrollment, GradeData } from "@/lib/types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
+import Loader from "@/components/Loader";
 
 type Props = { params: { id: string } };
 
@@ -13,6 +14,8 @@ export default function Index({ params }: Props) {
   const [classCode, setClassCode] = useState("");
   const [enrollments, setEnrollments] = useState<Enrollment[] | null>(null);
   const [selectedRow, setSelectedRow] = useState<GradeData | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const getEnrollments = async () => {
@@ -34,48 +37,63 @@ export default function Index({ params }: Props) {
       setClassCode(code);
     };
 
-    getEnrollments();
-    getClassCode();
+    Promise.all([getEnrollments(), getClassCode()])
+      .catch(() => setError("Something went wrong"))
+      .finally(() => setIsLoading(false));
   }, [supabase, params.id]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <div className="flex h-screen items-center justify-center">{error}</div>
+    );
+  }
 
   return (
     <div className="max-w-6xl mx-auto">
       <div className="flex justify-between items-center mb-11">
-        {classCode ? (
-          <h2 className="font-bold text-2xl">{classCode} Students</h2>
-        ) : null}
+        <h2 className="font-bold text-2xl">{classCode} Students</h2>
       </div>
 
       <section className="mt-10 p-7 bg-secondary rounded-2xl shadow-md">
         <h3 className="font-semibold text-lg">All Students</h3>
 
-        <div className="overflow-x-auto overflow-y-scroll mt-7 max-h-[650px]">
-          <table className="table">
-            <thead>
-              <tr className="text-primary invert-[.1] border-0 text-base">
-                <th>Total Marks</th>
-                <th>Student Name</th>
-                <th>Student Email</th>
-                <th>Grade</th>
-                <th>Action</th>
-              </tr>
-            </thead>
+        {enrollments?.length ? (
+          <div className="overflow-x-auto overflow-y-scroll mt-7 max-h-[650px]">
+            <table className="table">
+              <thead>
+                <tr className="text-primary invert-[.1] border-0 text-base">
+                  <th>Total Marks</th>
+                  <th>Student Name</th>
+                  <th>Student Email</th>
+                  <th>Grade</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {enrollments ? (
-                <>
-                  {enrollments.map((enrollment: Enrollment) => (
-                    <ClassDetailsTableRow
-                      key={enrollment.id}
-                      data={enrollment}
-                      onSelectRow={(data: GradeData) => setSelectedRow(data)}
-                    />
-                  ))}
-                </>
-              ) : null}
-            </tbody>
-          </table>
-        </div>
+              <tbody>
+                {enrollments ? (
+                  <>
+                    {enrollments.map((enrollment: Enrollment) => (
+                      <ClassDetailsTableRow
+                        key={enrollment.id}
+                        data={enrollment}
+                        onSelectRow={(data: GradeData) => setSelectedRow(data)}
+                      />
+                    ))}
+                  </>
+                ) : null}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div className="flex h-80 items-center justify-center">
+            No students
+          </div>
+        )}
       </section>
 
       {selectedRow ? (
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-screen -mt-4">
+      <span className="loading loading-ring loading-lg"></span>
+    </div>
+  );
+}
